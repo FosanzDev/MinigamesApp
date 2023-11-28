10058bc21288020c49acee9a60538f4fdbf99cfa
Victor <rusvictor@hotmail.es>
2023-11-29 00:13:44 +0100

    Comprobacion de que la entrada de editText es solo una letra mediante if que usa un metodo de LogicaAhorcado que devuelve verdadero o falsodependiendo de si solo hay un caracter intoducido usando el .length.
    Uso de stringbuilder y setlength 0 para recoger el contenido de edit text
    
    Uso de Toast para comprobaciones de los if para mostrar al usuario que la longitud es correcta o incorrecta.

--- app/src/main/java/com/fosanzdev/minigamesapp/logicaJuegos/LogicaAhorcado.java
+++ app/src/main/java/com/fosanzdev/minigamesapp/logicaJuegos/LogicaAhorcado.java
@@ -1,4 +1,10 @@
 package com.fosanzdev.minigamesapp.logicaJuegos;
 
 public class LogicaAhorcado {
+    public static boolean LetraUnica(String entradaString){
+        if(entradaString.length() != 1){
+            return  false;
+        }
+        return true;
+    }
 }
