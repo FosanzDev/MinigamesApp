aa38d2ec4b3bcd39e2ce7d2e1ad2bdac470eaf5e
Victor <rusvictor@hotmail.es>
2023-12-02 21:36:48 +0100

    Completando el juego con cambio del imagen dependiendo de cuantos intentos tiene el jugador usando varios if para comprobar el numIntentos

--- app/src/main/java/com/fosanzdev/minigamesapp/fragmentos/FragmentAhorcado.java
+++ app/src/main/java/com/fosanzdev/minigamesapp/fragmentos/FragmentAhorcado.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +23,8 @@ import java.util.Scanner;
 
 public class FragmentAhorcado extends Fragment {
     private final ArrayList<String> listaPalabras = new ArrayList<>();
+    private boolean jugar;
+    private boolean victoria;
 
     public FragmentAhorcado(){
         super(R.layout.fragment_ahorgado);
@@ -39,13 +42,15 @@ public class FragmentAhorcado extends Fragment {
         TextView tvPalbaraSecreta= view.findViewById(R.id.tvPalabraSecreta);
         TextView tvIntentos = view.findViewById(R.id.tvIntentos);
         TextView tvLetras = view.findViewById(R.id.tvLetras);
-        String intentosOrgininal = "6";
+        final int[] numIntentos = {6};
+        final String[] textNumeIntentos = {String.valueOf(numIntentos[0])};
         String letrasOriginales ="";
+        ImageView ivAhorcadoEstadoImage = view.findViewById(R.id.imageView);
         //Inicio del juego donde cogemos la palabra de la lsita
         //Y convertimos/ocultamos la palabra
         String palabraAleatoria = LogicaAhorcado.eliminarAcentos(getPalabraDeLista());
         final String[] palabaraCodigo = {LogicaAhorcado.ocultarPalabraSecreta(palabraAleatoria.length())};
-        tvIntentos.setText(intentosOrgininal);
+        tvIntentos.setText(textNumeIntentos[0]);
         tvLetras.setText(letrasOriginales);
         tvPalbaraSecreta.setText(palabaraCodigo[0]);
 
@@ -55,43 +60,72 @@ public class FragmentAhorcado extends Fragment {
         final String[] letra = new String[1];
         ArrayList<String> letrasUsadas = new ArrayList<>();
         final ArrayList<Integer>[] postionDeLetras = new ArrayList[]{new ArrayList<>()};
+        jugar = true;
+        victoria = false;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
                 sbLetra.append(etLetra.getText());
                 letra[0] = sbLetra.toString();
                 letra[0] = letra[0].toLowerCase();
                 letra[0] = LogicaAhorcado.eliminarAcentos(letra[0]);
+                if(numIntentos[0] == 0 && !victoria) {
+                    Toast.makeText(getActivity(), "No tienes mas Intentos", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Ha perdido", Toast.LENGTH_SHORT).show();
 
-                if(LogicaAhorcado.characterUnico(letra[0])) {
-                    //
-                    if(LogicaAhorcado.siLetraEnLista(letrasUsadas,letra[0])){
-                        Toast.makeText(getActivity(), "Este letra ya esta usada", Toast.LENGTH_SHORT).show();
-                    }else{
-                        letrasUsadas.add(letra[0]);
-                        tvLetras.setText(letrasUsadas.toString());
-                        if(LogicaAhorcado.confirmarLetra(letra[0])){
-                            if(LogicaAhorcado.confirmalSiHayLetra(palabraAleatoria, letra[0])){
-                                postionDeLetras[0] = LogicaAhorcado.getPosLetra(palabraAleatoria, letra[0]);
-                                //DELETE
-                                Toast.makeText(getActivity(), palabraAleatoria, Toast.LENGTH_SHORT).show();
-                                //
-                                palabaraCodigo[0] = LogicaAhorcado.mostrarLetraCorrecta(palabaraCodigo[0], postionDeLetras[0],letra[0]);
-                                tvPalbaraSecreta.setText(palabaraCodigo[0]);
-                            }else{
-                                Toast.makeText(getActivity(), "Este letra no existe", Toast.LENGTH_SHORT).show();
-                                //TODO cambiar el imagen cuando la letra es incorecta
-                            }
-                        }else
-                            Toast.makeText(getActivity(), "El caharcter intorducido no es una letra", Toast.LENGTH_SHORT).show();
-                    }
-                }else
-                    Toast.makeText(getActivity(), "Tenies que introducir UNA letra", Toast.LENGTH_SHORT).show();
+                }else if(!jugar){
+                    Toast.makeText(getActivity(), "Has ganado", Toast.LENGTH_SHORT).show();
+                }else{
+                    if(LogicaAhorcado.characterUnico(letra[0])) {
+                        //
+                        if(LogicaAhorcado.siLetraEnLista(letrasUsadas,letra[0])){
+                            Toast.makeText(getActivity(), "Este letra ya esta usada", Toast.LENGTH_SHORT).show();
+                        }else{
+                            letrasUsadas.add(letra[0]);
+                            tvLetras.setText(letrasUsadas.toString());
+                            if(LogicaAhorcado.confirmarLetra(letra[0])){
+                                if(LogicaAhorcado.confirmalSiHayLetra(palabraAleatoria, letra[0])){
+                                    postionDeLetras[0] = LogicaAhorcado.getPosLetra(palabraAleatoria, letra[0]);
+                                    //DELETE
+                                    Toast.makeText(getActivity(), palabraAleatoria, Toast.LENGTH_SHORT).show();
+                                    //
+                                    palabaraCodigo[0] = LogicaAhorcado.mostrarLetraCorrecta(palabaraCodigo[0], postionDeLetras[0],letra[0]);
+                                    tvPalbaraSecreta.setText(palabaraCodigo[0]);
+                                    if(palabaraCodigo[0].equals(palabraAleatoria)){
+                                        Toast.makeText(getActivity(), "HAS GANADO", Toast.LENGTH_SHORT).show();
+                                        jugar = false;
+                                    }
+                                }else{
+                                    Toast.makeText(getActivity(), "Este letra no existe en la palabra", Toast.LENGTH_SHORT).show();
+                                    numIntentos[0] -= 1;
+                                    textNumeIntentos[0] = String.valueOf(numIntentos[0]);
+                                    tvIntentos.setText(textNumeIntentos[0]);
+                                    if(numIntentos[0] == 0 ) {
+                                        Toast.makeText(getActivity(), "No tienes mas Intentos", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getActivity(), "GAME OVER", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getActivity(), "Para jugar otra vez pula el boton de reinicio", Toast.LENGTH_SHORT).show();
+                                    }
+                                    if(numIntentos[0] == 5)
+                                        ivAhorcadoEstadoImage.setImageResource(R.drawable.hangman_5);
+                                    if(numIntentos[0] == 4)
+                                        ivAhorcadoEstadoImage.setImageResource(R.drawable.hangman_4);
+                                    if(numIntentos[0] == 3)
+                                        ivAhorcadoEstadoImage.setImageResource(R.drawable.hangman_3);
+                                    if(numIntentos[0] == 2)
+                                        ivAhorcadoEstadoImage.setImageResource(R.drawable.hangman_2);
+                                    if(numIntentos[0] == 1)
+                                        ivAhorcadoEstadoImage.setImageResource(R.drawable.hangman_1);
+                                    if(numIntentos[0] == 0)
+                                        ivAhorcadoEstadoImage.setImageResource(R.drawable.hangman_0);
 
-                //if()
-                //else
+                                }
+                            }else
+                                Toast.makeText(getActivity(), "El caracter intorducido no es una letra", Toast.LENGTH_SHORT).show();
+                        }
+                    }else
+                        Toast.makeText(getActivity(), "Tenies que introducir UNA letra", Toast.LENGTH_SHORT).show();
+                }
+
                 sbLetra.setLength(0);
                 postionDeLetras[0].clear();
             }
--- app/src/main/java/com/fosanzdev/minigamesapp/logicaJuegos/LogicaAhorcado.java
+++ app/src/main/java/com/fosanzdev/minigamesapp/logicaJuegos/LogicaAhorcado.java
@@ -1,24 +1,17 @@
 package com.fosanzdev.minigamesapp.logicaJuegos;
-
 import java.util.ArrayList;
-
 public class LogicaAhorcado {
-
     public static boolean characterUnico(String entradaString){
         return entradaString.length() == 1;
     }
-
-
     public static boolean confirmarLetra(String letra){
         String consonants="bcdfghjklmnñpqrstvwxyz";
         String vocales="aáàeéèiíìoóòuúù";
         return consonants.contains(letra) || vocales.contains(letra);
     }
-
     public static boolean confirmalSiHayLetra(String palabra,String letra){
         return palabra.contains(letra);
     }
-
     public static String eliminarAcentos(String texto){
         String origen="áàéèíìóòúù";
         String destino="aaeeiioouu";
@@ -27,7 +20,6 @@ public class LogicaAhorcado {
         }
         return texto;
     }
-
     public static String ocultarPalabraSecreta(int longitud){
         StringBuilder palabra = new StringBuilder();
         for(int i = 0; i < longitud;i++){
@@ -35,7 +27,6 @@ public class LogicaAhorcado {
         }
         return palabra.toString();
     }
-
     public static ArrayList<Integer> getPosLetra(String palbaraSecreta, String letra){
         ArrayList<Integer> posLetras = new ArrayList<>();
         for(int i = 0; i < palbaraSecreta.length(); i++){
@@ -46,7 +37,6 @@ public class LogicaAhorcado {
         }
         return posLetras;
     }
-
     public static String mostrarLetraCorrecta(String palabaraOcultada,ArrayList<Integer> posLetras, String letrra){
             char[] charArray = palabaraOcultada.toCharArray();
             char letraAChar = letrra.charAt(0);
@@ -57,9 +47,7 @@ public class LogicaAhorcado {
             }
             return new String(charArray);
     }
-
     public static boolean siLetraEnLista(ArrayList<String> arrayDeLetrasUsadas,String letra){
         return arrayDeLetrasUsadas.contains(letra);
     }
-
 }
