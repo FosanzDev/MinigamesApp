355b29d6f372e901accef355793ef3a55f169ef9
Esteban Sanchez Llobregat <108540114+FosanzDev@users.noreply.github.com>
2023-12-02 19:44:16 +0100

    Project Sync

--- app/src/main/java/com/fosanzdev/minigamesapp/MainActivity.java
+++ app/src/main/java/com/fosanzdev/minigamesapp/MainActivity.java
@@ -3,21 +3,33 @@ package com.fosanzdev.minigamesapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+import com.fosanzdev.minigamesapp.fragmentos.FragmentAhorcado;
+import com.fosanzdev.minigamesapp.fragmentos.FragmentStart;
+import com.fosanzdev.minigamesapp.fragmentos.FragmentTicTacToe;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    FragmentManager manager;
+    FragmentTransaction fragmentTransaction;
+    Fragment fragmentToCome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.flMainView, new FragmentStart());
+        fragmentTransaction.commit();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,7 +39,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        manager = getSupportFragmentManager();
         int itemID = item.getItemId();
+        if(itemID == R.id.action_hangman){
+            fragmentToCome = new FragmentAhorcado();
+            cambiarFragment(fragmentToCome);
+            return  true;
+        }else if(itemID == R.id.action_tictac){
+
+        }
         return false;
+    }
+    private void cambiarFragment(Fragment fragment){
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.flMainView, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/fosanzdev/minigamesapp/fragmentos/FragmentAhorcado.java
+++ app/src/main/java/com/fosanzdev/minigamesapp/fragmentos/FragmentAhorcado.java
@@ -9,15 +9,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.fosanzdev.minigamesapp.R;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class FragmentAhorcado extends Fragment {
-
-
+    private final ArrayList<String> listaPalabras = new ArrayList<>();
 
     public FragmentAhorcado(){
-
+        super(R.layout.fragment_ahorgado);
     }
 
     @Override
@@ -25,11 +33,47 @@ public class FragmentAhorcado extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ahorgado, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        crearListaDePalabras();
+        TextView tvPalbaraSecreta= view.findViewById(R.id.tvPalabraSecreta);
+        TextView tvIntentos = view.findViewById(R.id.tvIntentos);
+        TextView tvLetras = view.findViewById(R.id.tvLetras);
+        String intentosOrgininal = "6";
+        String letrasOriginales ="";
+        //Inicio del juego donde cogemos la palabra de la lsita
+        //Y convertimos/ocultamos la palabra
+        String palabraAleatoria = getPalabraDeLista();
+        String palabaraCodigo =ocultarPalabraSecreta(palabraAleatoria.length());
+        tvIntentos.setText(intentosOrgininal);
+        tvLetras.setText(letrasOriginales);
+        tvPalbaraSecreta.setText(palabaraCodigo);
+    }
+
+    public void crearListaDePalabras(){
+        InputStream file = getResources().openRawResource(R.raw.words_es);
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            listaPalabras.add(s);
+        }
+    }
+    public String getPalabraDeLista(){
+        Random rand = new Random();
+        int min = 0;
+        int max = listaPalabras.size();
+
+        int posAleatoria = rand.nextInt(max - min +1)+min;
+        return listaPalabras.get(posAleatoria);
+    }
+
+    public String ocultarPalabraSecreta(int longitud){
+        StringBuilder palabra = new StringBuilder();
+        for(int i = 0; i < longitud;i++){
+            palabra.append("_");
+        }
+        return palabra.toString();
     }
 
 }
